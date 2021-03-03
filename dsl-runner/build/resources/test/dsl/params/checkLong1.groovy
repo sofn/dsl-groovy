8fe0abb0fec1c4d9158837fef95d897ece37109e
sofn <lishaofeng02@meituan.com>
2021-03-03 16:28:25 +0800

    func(gradle): delete unused file

--- dsl-runner/build/resources/test/dsl/params/checkLong1.groovy
+++
@@ -1,9 +0,0 @@
-package dsl.params
-
-import dsl.groovy.DslDelegate
-import groovy.transform.BaseScript
-
-@BaseScript DslDelegate _
-
-par1 = "0"
-longParam(par1, checks = [min: 1, max: 3])
--- dsl-runner/build/resources/test/dsl/params/checkLong2.groovy
+++
@@ -1,9 +0,0 @@
-package dsl.params
-
-import dsl.groovy.DslDelegate
-import groovy.transform.BaseScript
-
-@BaseScript DslDelegate _
-
-par1 = "1"
-longParam(par1, checks = [min: 2, max: 3])
